8c56690ca6c78a5925e82abeb99b43772baf3bea
Nasiru-code <akingbolahan12@gmail.com>
2022-04-15 12:37:04 +0100

    added memeber registeration function

--- become.php
+++ become.php
@@ -1,105 +1,13 @@
-<!doctype html>
-<html lang="zxx">
-
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>National Shea Products Association of Nigeria Official website</title>
-  <!-- google fonts -->
-  <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap" rel="stylesheet">
-  <!-- google fonts -->
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="assets/css/style-liberty.css">
-  <link rel="stylesheet" href="assets/css/custom.css">
-  <!-- Template CSS -->
-</head>
-
+<?php 
+session_start();
+include('includes/config.php');
+include('includes/header.php');
+?>
 <body>
   <!--header-->
-  <header id="site-header" class="fixed-top">
-    <div class="container">
-      <nav class="navbar navbar-expand-lg stroke">
-        <h1><a class="navbar-brand mr-lg-5" href="index">
-            <img src="assets/images/naspan_logo.jpg" style="height: 40px;">
-            NAS<span>PAN</span>
-          </a></h1>
-        <!-- if logo is image enable this   
-      <a class="navbar-brand" href="#index">
-          <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
-      </a> -->
-        <button class="navbar-toggler  collapsed bg-gradient" type="button" data-toggle="collapse"
-          data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
-          <span class="navbar-toggler-icon fa icon-close fa-times"></span>
-          </span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                About<span class="fa fa-angle-down"></span>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                <a class="dropdown-item" href="about">About Us</a>
-                <a class="dropdown-item" href="industry_overview">Industry Overview</a>
-                <a class="dropdown-item" href="executives">Executive Members</a>
-                <a class="dropdown-item" href="strategic_focus">Strategic Focus</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                Media <span class="fa fa-angle-down"></span>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                <a class="dropdown-item" href="blog">News & Event</a>
-                <a class="dropdown-item" href="gallery">Gallery</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact">Contact</a>
-            </li>
-            <li class="nav-item active dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                Member Area <span class="fa fa-angle-down"></span>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                <a class="dropdown-item" href="signin">Sign In</a>
-                <a class="dropdown-item" href="become">Register</a>
-                <a class="dropdown-item" href="membership">Membership</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="d-lg-block d-none">
-          <a href="become" class="btn btn-style btn-secondary ml-lg-3">Become a Member</a>
-        </div>
-        <!-- toggle switch for light and dark theme -->
-        <div class="mobile-position">
-          <nav class="navigation">
-            <div class="theme-switch-wrapper">
-              <label class="theme-switch" for="checkbox">
-                <input type="checkbox" id="checkbox">
-                <div class="mode-container">
-                  <i class="gg-sun"></i>
-                  <i class="gg-moon"></i>
-                </div>
-              </label>
-            </div>
-          </nav>
-        </div>
-        <!-- //toggle switch for light and dark theme -->
-      </nav>
-    </div>
-  </header>
+  <?php
+  include('includes/nav.php');
+  ?>
   <!-- //header -->
   <!-- about breadcrumb -->
   <section class="w3l-about-breadcrumb text-left">
@@ -130,6 +38,7 @@
               <input type="text" name="phone" id="phone" placeholder="Phone Number" class="contact-input" />
               <input type="email" name="email" id="email" placeholder="Your email address" class="contact-input"
                 required />
+                <div id="emailStatus"></div>
             </div>
             <div class="d-grid">
               <select class="form-control">
@@ -209,134 +118,41 @@
   <!--//contact-->
 
   <!--/w3l-footer-29-main-->
-  <section class="w3l-footer-29-main">
-    <div class="footer-29 py-5">
-      <div class="container py-lg-4">
-        <div class="row footer-top-29">
-          <div class="footer-list-29 col-lg-6">
-            <h6 class="footer-title-29">NASPAN</h6>
-            <p class="pr-lg-5">Connect with us across different platforms.</p>
-            <div class="main-social-footer-29 mt-4">
-              <a href="#facebook" class="facebook"><span class="fa fa-facebook"></span></a>
-              <a href="#twitter" class="twitter"><span class="fa fa-twitter"></span></a>
-              <a href="#instagram" class="instagram"><span class="fa fa-instagram"></span></a>
-              <a href="#linkedin" class="linkedin"><span class="fa fa-linkedin"></span></a>
-            </div>
-          </div>
-          <div class="col-lg-6 col-md-6 col-sm-8 footer-list-29 footer-1 mt-lg-0 mt-5">
-            <h6 class="footer-title-29">Contact Us</h6>
-            <ul>
-              <li>
-                <p><span class="fa fa-map-marker"></span> NASPAN, #3 Femi Okunnu close off Augustus Aikhomu Way,
-                  Utako-Abuja
-                </p>
-              </li>
-              <li><a href="tel:+234 913-714-7993"><span class="fa fa-phone"></span> +(234) 913-714-7993</a> <a
-                  href="mailto:naspansecretariat@gmail.com" class="mail"><span class="fa fa-envelope-open-o"></span>
-                  naspansecretariat@gmail.com</a> </li>
-            </ul>
-          </div>
+  <!--//w3l-bottom-->
 
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <section class="w3l-footer-29-main w3l-copyright">
-    <div class="container">
-      <div class="bottom-copies">
-        <p class="copy-footer-29 text-center">© 2022 NASPAN. All rights reserved.
-        </p>
-      </div>
-    </div>
-
-    <!-- move top -->
-    <button onclick="topFunction()" id="movetop" title="Go to top">
-      <span class="fa fa-angle-up"></span>
-    </button>
-    <script>
-      // When the user scrolls down 20px from the top of the document, show the button
-      window.onscroll = function () {
-        scrollFunction()
-      };
-
-      function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          document.getElementById("movetop").style.display = "block";
-        } else {
-          document.getElementById("movetop").style.display = "none";
-        }
-      }
-
-      // When the user clicks on the button, scroll to the top of the document
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
-
-    </script>
-    <!-- /move top -->
-  </section>
+  <!--/w3l-footer-29-main-->
+  <?php
+  include('includes/footer.php');
+  ?>
   <!-- //footer-29 block -->
-  <!-- disable body scroll which navbar is in active -->
+  <?php
+  include('includes/scripts.php');
+  ?>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script>
-    $(function () {
-      $('.navbar-toggler').click(function () {
-        $('body').toggleClass('noscroll');
-      })
-    });
-  </script>
-  <!-- disable body scroll which navbar is in active -->
-
-  <!-- Template JavaScript -->
-  <script src="assets/js/jquery-3.3.1.min.js"></script>
-  <script src="assets/js/theme-change.js"></script>
-  <!--/MENU-JS-->
-  <script>
-    $(window).on("scroll", function () {
-      var scroll = $(window).scrollTop();
-
-      if (scroll >= 80) {
-        $("#site-header").addClass("nav-fixed");
-      } else {
-        $("#site-header").removeClass("nav-fixed");
+    $(document).on('input','#email',function(e){
+      let emailInput = $('#email').val();
+      let msg;
+      if(emailInput.length==0){
+        msg="<span style='color:red'>Enter Email</span>";
       }
+      // else if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(emailInput)){
+      //   msg="<span style='color:red'>Email is not Valid</span>";
+      // }
+      else{
+        checkEmail(emailInput);
+      }
+      $('#emailStatus').html(msg);
     });
 
-    //Main navigation Active Class Add Remove
-    $(".navbar-toggler").on("click", function () {
-      $("header").toggleClass("active");
-    });
-    $(document).on("ready", function () {
-      if ($(window).width() > 991) {
-        $("header").removeClass("active");
-      }
-      $(window).on("resize", function () {
-        if ($(window).width() > 991) {
-          $("header").removeClass("active");
+    function checkEmail(emailInput){
+       $.ajax({
+        method:"POST",
+        url: "email_exist.php",
+        data:{emailId:emailInput},
+        success: function(data){
+          $('#emailStatus').html(data);
         }
       });
-    });
-
-    // Password Confirm
-    var password = document.getElementById("password")
-      , confirm_password = document.getElementById("cpassword");
-
-    function validatePassword() {
-      if (password.value != confirm_password.value) {
-        confirm_password.setCustomValidity("Passwords Don't Match");
-      } else {
-        confirm_password.setCustomValidity('');
-      }
     }
-
-    password.onchange = validatePassword;
-    confirm_password.onkeyup = validatePassword;
   </script>
-  <!--//MENU-JS-->
-
-  <script src="assets/js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
